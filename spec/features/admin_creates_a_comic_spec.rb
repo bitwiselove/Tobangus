31af56fa6f644f4ce23225137dc06e26476c2044
Brandon Newton <bitwiselove@gmail.com>
2016-03-05 13:38:34 -0800

    Admin Creates Comics
    Admin users may sign in and create new comics with preview images.

--- spec/features/admin_creates_a_comic_spec.rb
+++ spec/features/admin_creates_a_comic_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
 
 feature 'Admin creates a comic' do
-  scenario 'Successfully' do
+  scenario 'Without being logged in' do
+    visit admin_root_path
+    expect(page).to have_content('Please sign in to continue.')
+  end
 
+  scenario 'Successfully' do
+    admin = create(:admin)
+    comic = build(:budsuckers)
+
+    visit admin_root_path(as: admin)
+
+    click_link 'New Comic'
+    fill_in 'Series', with: comic.series
+    fill_in 'Title', with: comic.title
+    fill_in 'Description', with: comic.description
+    attach_file 'Preview', "#{Rails.root}/spec/features/files/test_strip.png"
+    click_button 'Create Comic'
+
+    expect(page).to have_selector("input[value='#{comic.series}']")
+    expect(page).to have_selector("input[value='#{comic.title}']")
+    expect(page).to have_selector('textarea', text: comic.description)
+    expect(page).to have_selector('img')
   end
 end
